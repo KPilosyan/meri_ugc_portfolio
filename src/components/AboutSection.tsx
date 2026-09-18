351316bf62db3e7028a18386c78cb4a07074e081
Karen Pilosyan <pilosyan.karen@gmail.com>
2026-09-18 20:50:43 +0400

    Delete local media & upload to vercel

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -64,7 +64,7 @@ export const AboutSection: React.FC = () => {
         >
           <div className="relative w-full h-full bg-ugc-sand shadow-lg overflow-hidden">
             <Image
-              src="/images/k.png"
+              src="https://rrxt4mgceuhmmfet.public.blob.vercel-storage.com/k.png"
               alt="Mary Zatikian Editorial Portrait"
               fill
               className="object-cover"
